56189125fbc9d1a986b997f11cb4476eb0809849
ypykyns <ypykys@hotmail.com>
2022-09-27 08:17:46 -0300

    valida se os objetos vieram com valores preenchidos

--- aeroapi/Service.cs
+++ aeroapi/Service.cs
@@ -9,13 +9,14 @@ namespace aeroapi
     public class Service
     {
         Conn conn = new Conn();
+        UtilsWS utils = new UtilsWS();
 
         public string getPassageiro(int id)
         {
             JObject result = new JObject();
             try
-            {                              
-                SqlDataReader dr = conn.ExecuteQueryGetOrDelete("consultaPassageiro",id);
+            {
+                SqlDataReader dr = conn.ExecuteQueryGetOrDelete("consultaPassageiro", id);
 
                 if (dr.Read())
                 {
@@ -40,26 +41,39 @@ namespace aeroapi
                 result.Add("Message", "Erro ao consultar passageiro! - " + ex.Message);
                 return result.ToString();
             }
-        }      
-        
+        }
+
         public string postPassageiro(Object receivedInfo)
         {
             JObject result = new JObject();
-            try
-            {            
-                int modified = conn.ExecuteQueryPost("criarPassageiro", JObject.Parse(receivedInfo.ToString()));
 
-                if (modified > 0)
+            try
+            {
+                // valida se o objeto recebido está no formato correto, e sem valores vazios ou nulos
+                if (utils.validaObjPost(JObject.Parse(receivedInfo.ToString())))
                 {
-                    conn.Close();
-                    result.Add("Result", "Passageiro criado - ID: " + modified);
+
+                    int modified = conn.ExecuteQueryPost("criarPassageiro", JObject.Parse(receivedInfo.ToString()));
+
+                    if (modified > 0)
+                    {
+                        conn.Close();
+                        result.Add("Result", "Passageiro criado - ID: " + modified);
+                        return result.ToString();
+                    }
+                    else
+                    {
+                        conn.Close();
+                        result.Add("Result", "Falha ao tentar criar passageiro");
+                        return result.ToString();
+                    }
+                }
+                else
+                {
+                    result.Add("Result", "Verifique a sua requisão - Valores nulos ou vazios.");
                     return result.ToString();
                 }
-                else {
-                    conn.Close();
-                    result.Add("Result", "Falha ao tentar criar passageiro");
-                    return result.ToString();
-                }               
+
             }
             catch (Exception ex)
             {
@@ -68,19 +82,30 @@ namespace aeroapi
                 return result.ToString();
             }
 
-        }        
+        }
 
         public string putPassageiro(Object receivedInfo)
         {
             JObject result = new JObject();
             try
-            {           
-               SqlDataReader dr = conn.ExecuteQueryPut("atualizarPassageiro", JObject.Parse(receivedInfo.ToString()));
+            {
+                // valida se o objeto recebido está no formato correto, e sem valores vazios ou nulos
+                if (utils.validaObjPut(JObject.Parse(receivedInfo.ToString())))
+                {
 
-                result.Add("Result", dr.RecordsAffected == 1 ? "Passageiro alterado com sucesso!" : "Passageiro não encontrado com esse id.");
+                    SqlDataReader dr = conn.ExecuteQueryPut("atualizarPassageiro", JObject.Parse(receivedInfo.ToString()));
 
-                conn.Close();
-                return result.ToString();
+                    result.Add("Result", dr.RecordsAffected == 1 ? "Passageiro alterado com sucesso!" : "Passageiro não encontrado com esse id.");
+
+                    conn.Close();
+                    return result.ToString();
+
+                }
+                else
+                {
+                    result.Add("Result", "Verifique a sua requisão - Valores nulos ou vazios.");
+                    return result.ToString();
+                }
             }
             catch (Exception ex)
             {
@@ -88,15 +113,15 @@ namespace aeroapi
                 result.Add("Result", "Falha ao alterar passageiro - " + ex.Message);
                 return result.ToString();
             }
-        }        
+        }
 
         public string deletePassageiro(int id)
         {
             JObject result = new JObject();
             try
             {
-                SqlDataReader dr = conn.ExecuteQueryGetOrDelete("excluirPassageiro", id);              
-                              
+                SqlDataReader dr = conn.ExecuteQueryGetOrDelete("excluirPassageiro", id);
+
                 result.Add("Message", dr.RecordsAffected == 1 ? "Passageiro excluído com sucesso!" : "Passageiro não encontrado com esse id.");
                 conn.Close();
 
@@ -108,6 +133,6 @@ namespace aeroapi
                 result.Add("Message", "Falha ao excluir passageiro - " + ex.Message);
                 return result.ToString();
             }
-        }        
+        }
     }
 }
